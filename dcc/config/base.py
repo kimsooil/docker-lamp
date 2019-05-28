a37f6e255920ec62bd3bde4456731bc135e452d3
Elastic <swanjie3@yahoo.com>
2019-05-28 16:51:37 -0400

    Prod docker-compose.

--- dcc/config/base.py
+++ dcc/config/base.py
@@ -260,9 +260,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/"),
     '/var/www/static/',
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = '/media/' if ENV('ENVIRONMENT') == 'production' else os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
--- dcc/urls.py
+++ dcc/urls.py
@@ -49,6 +49,8 @@ urlpatterns = [
     path('drf/not-authenticated/', ExampleView.as_view()),
     path('drf/authenticated/', ExampleAuthenticatedView.as_view())
 ] + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT
+) + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
 
