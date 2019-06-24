e9a1023006bf36e51ac04ef88b8756fc60d15044
Elastic <swanjie3@yahoo.com>
2019-06-24 15:59:18 -0400

    Adding the Django create admin.

--- dcc/management/commands/init_admin_user.py
+++ dcc/management/commands/init_admin_user.py
@@ -1,15 +1,32 @@
 from django.core.management.base import BaseCommand, CommandError
 
-# import environ
-# env = environ.Env()
+import environ
+env = environ.Env()
 
-# from users.models import User
+from users.models import User
 
 class Command(BaseCommand):
     help = 'Displays current time'
 
     def handle(self, *args, **kwargs):
-        self.stdout.write(self.style.SUCCESS('???????????????????????????????????????????????????????????'))
-        # email=env('ADMIN_EMAIL', default=None)
+        email=env('ADMIN_EMAIL', default=None)
 
-        # self.stdout.write("Is this the email?????????????????????????????????????????:::: %s" % email)
+        if email:
+            if not User.objects.filter(email=email).exists():
+                user = User.objects.create_user(
+                    is_staff=True,
+                    is_superuser=True,
+                    username=env('ADMIN_USERNAME', default='admin'),
+                    email=email,
+                    password=env('ADMIN_PASSWORD')
+                )
+                self.stdout.write(self.style.SUCCESS('DCC: Admin user created!'))
+            elif bool(env('ADMIN_RESET', default=False)):
+                user = User.objects.get(email=email)
+                user.set_password(env('ADMIN_PASSWORD'))
+                user.save()
+                self.stdout.write(self.style.SUCCESS('DCC: Admin password reset!'))
+            else:
+                self.stdout.write(self.style.SUCCESS('DCC: Admin user already exists!'))
+        else:
+            self.stdout.write(self.style.WARNING('DCC: Did not create admin user!'))
