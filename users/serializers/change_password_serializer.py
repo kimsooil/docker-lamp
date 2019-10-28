57ebccdce00a32f3b9a8c50fb06dce7b924310eb
Joseph McCombs <jmccombs@nd.edu>
2019-10-28 14:48:33 -0400

    set Old_PASSWORD_FIELD_ENABLED to true, to catch incorrect old passwords

--- users/serializers/change_password_serializer.py
+++ users/serializers/change_password_serializer.py
@@ -1,16 +1,10 @@
 from django.contrib.auth import get_user_model, authenticate
 from django.conf import settings
-from django.contrib.auth.forms import SetPasswordForm # PasswordResetForm
-# from django.contrib.auth.tokens import default_token_generator
-# from django.utils.http import urlsafe_base64_decode as uid_decoder
+from django.contrib.auth.forms import SetPasswordForm 
 from django.utils.translation import ugettext_lazy as _
-# from django.utils.encoding import force_text
 
 from rest_framework import serializers, exceptions
 from rest_framework.exceptions import ValidationError
-
-# from .models import TokenModel
-# from .utils import import_callable
 
 # Get the UserModel
 UserModel = get_user_model()
