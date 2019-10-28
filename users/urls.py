c28e51acf8eef1561e4637bb4ce3f2343dbe326d
Elastic <swanjie3@yahoo.com>
2019-10-28 16:59:04 -0400

    Cleanup.

--- users/urls.py
+++ users/urls.py
@@ -17,32 +17,75 @@ import oauth2_provider.views as oauth2_views
 
 app_name = "users"
 urlpatterns = [
-    # path("", view=user_list_view, name="list"),
-
-    # Authentication/Authorization/Revoke paths.
-    # path('api/authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
-    path('api/token/', oauth2_views.TokenView.as_view(), name="token"),
-    path('api/revoke-token/', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
-
+    # Get Authentication Token.
+    path(
+        'api/token/',
+        oauth2_views.TokenView.as_view(),
+        name="token"
+    ),
+    # Revoke Token (i.e. logout).
+    path(
+        'api/revoke-token/',
+        oauth2_views.RevokeTokenView.as_view(),
+        name="revoke-token"
+    ),
     # Login/LOgout paths.
-    path("api/login/", view=LoginAPIView.as_view(), name="api-login"),
-    path("api/logout/", view=LogoutAPIView.as_view(), name="api-logout-everywhere"),
-
+    path(
+        "api/login/",
+        view=LoginAPIView.as_view(),
+        name="api-login"
+    ),
+    path(
+        "api/logout/",
+        view=LogoutAPIView.as_view(),
+        name="api-logout-everywhere"
+    ),
     # Account and update paths.
-    path("api/me/", view=MeAPIView.as_view(), name="api-me"),
-    path("api/update/", view=UpdateMeAPIView.as_view(), name="api-update"),
-    
+    path(
+        "api/me/",
+        view=MeAPIView.as_view(),
+        name="api-me"
+    ),
+    path(
+        "api/update/",
+        view=UpdateMeAPIView.as_view(),
+        name="api-update"
+    ),
     # HTML Logout.
-    path("logout/", view=UserLogoutView.as_view(), name="logout"),
-
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_detail_view, name="detail"),
-    
-
+    path(
+        "logout/",
+        view=UserLogoutView.as_view(),
+        name="logout"
+    ),
+    path(
+        "~redirect/",
+        view=user_redirect_view,
+        name="redirect"
+    ),
+    path(
+        "~update/",
+        view=user_update_view,
+        name="update"
+    ),
+    path(
+        "<str:username>/",
+        view=user_detail_view,
+        name="detail"
+    ),
     # Forgot Password
-    path("api/forgot-password/", view=ForgotPasswordAPIView.as_view(), name="forgot-password"),
-    path('api/reset-password/', view=PasswordResetAPIView.as_view(), name='reset-password'),
-
-    path('api/change-password/', view=PasswordChangeAPIView.as_view(), name='change-password'),
+    path(
+        "api/forgot-password/",
+        view=ForgotPasswordAPIView.as_view(),
+        name="forgot-password"
+    ),
+    path(
+        'api/reset-password/',
+        view=PasswordResetAPIView.as_view(),
+        name='reset-password'
+    ),
+    path(
+        'api/change-password/',
+        view=PasswordChangeAPIView.as_view(),
+        name='change-password'
+    ),
 ]
