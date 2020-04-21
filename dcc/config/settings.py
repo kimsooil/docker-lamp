87d8ec2c97af1f84034be3db26e4560eaec326ff
Caleb Reinking <calebreinking@Kion.local>
2020-04-21 15:02:15 -0400

    pointing state to proper env variable

--- dcc/config/settings.py
+++ dcc/config/settings.py
@@ -13,7 +13,7 @@ ENV = environ.Env()
 
 MODEL_API_BASE_URL = ENV('MODEL_API_BASE_URL', default='http://localhost:5000/')
 MODEL_API_SUBPATH = ENV('MODEL_API_SUBPATH', default='/model/')
-MODEL_API_STATE = ENV('MODEL_API_SUBPATH', default='Nebraska')
+MODEL_API_STATE = ENV('MODEL_API_STATE', default='Nebraska')
 
 API_DEFAULT_COUNTIES = ENV.list('DJANGO_API_DEFAULT_COUNTIES', default=['Lincoln'])
 API_DEFAULT_MAP_X_COORD = ENV.float('DJANGO_API_DEFAULT_MAP_X_COORD', default=41.5)
