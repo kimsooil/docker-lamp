6070caeda1601637dea3cf506cf8083b1ed3c45e
Caleb Reinking <calebreinking@Kion.local>
2020-04-22 10:08:34 -0400

    changing map center due to float type bug in django-environ

--- covid/views.py
+++ covid/views.py
@@ -34,7 +34,10 @@ class SystemConfigurationAPIView(ProtectedResourceView, APIView):
             'state': settings.MODEL_API_STATE,
             'default_counties': settings.API_DEFAULT_COUNTIES,
             'map': {
-                'center' : [settings.API_DEFAULT_MAP_X_COORD, settings.API_DEFAULT_MAP_Y_COORD],
+                'center' : [
+                    float(settings.API_DEFAULT_MAP_X_COORD), 
+                    float(settings.API_DEFAULT_MAP_Y_COORD)
+                ],
                 'zoom' : settings.API_DEFAULT_MAP_ZOOM_LEVEL
             }
         }
