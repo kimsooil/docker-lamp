102628ddf53d7e0cb8abcefdfedc107a4b55e5f4
Caleb Reinking <calebreinking@Kion.local>
2020-04-28 01:10:25 -0400

    increasing DRF pagination

--- dcc/config/base.py
+++ dcc/config/base.py
@@ -237,7 +237,7 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.MultiPartParser',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50,
+    'PAGE_SIZE': 200,
 }
 
 # Email settings.
