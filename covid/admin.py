6b0b13b9507ab67ca250c35708fafc0fbf5549f8
Caleb Reinking <calebreinking@Kion.local>
2020-05-05 09:22:50 -0400

    inline editing of county resource data

--- covid/admin.py
+++ covid/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from .models import State, County
 
 admin.site.register(State)
-admin.site.register(County)
+
+class CountyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'state', 'hospital_bed_capacity', 'icu_bed_capacity', 'ventilator_capacity')
+    list_editable = ('hospital_bed_capacity', 'icu_bed_capacity', 'ventilator_capacity')
+    search_fields = ['name', 'state__name']
+
+admin.site.register(County, CountyAdmin)
 
