c670f4dc86ff9592c69e159ec311e39653c636aa
Caleb Reinking <calebreinking@Kion.local>
2020-05-07 15:21:05 -0400

    case insensitive usernames

--- users/models.py
+++ users/models.py
@@ -1,10 +1,16 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
+class CustomUserManager(UserManager):
+    def get_by_natural_key(self, username):
+        case_insensitive_username_field = '{}__iexact'.format(self.model.USERNAME_FIELD)
+        return self.get(**{case_insensitive_username_field: username})
+
 class User(AbstractUser):
+    objects = CustomUserManager()
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
