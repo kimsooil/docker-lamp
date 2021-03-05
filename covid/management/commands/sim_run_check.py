a658d187cc9d07d6cf4d2573f17726c51403170b
Optimus <swanjie3@yahoo.com>
2021-03-05 13:42:56 -0500

    Timestamp and docmments on runs.

--- covid/management/commands/sim_run_check.py
+++ covid/management/commands/sim_run_check.py
@@ -11,32 +11,46 @@ class Command(BaseCommand):
     help = 'Send a CSV with data on the most recent runs'
 
     def handle(self, *args, **options):
-        sim_list = SimulationRun.objects.all().order_by('-timestamp')[:500]
+        # Range of today.
+        start = datetime.now().replace(hour=0, minute=0, second=0)
+        end = datetime.now().replace(hour=23, minute=59, second=59)
+
+        # Get the CSV jobs.
+        sim_list = SimulationRun.objects.filter(
+            timestamp__range=[start, end]
+        ).order_by('-timestamp')
+
         rows = []
         git_hash = None
 
+        # Set the data for the CSV.
         for item in sim_list:
             rows.append({
                 'state': item.model_input['state'],
                 'counties': ",".join(item.model_input['county']),
-                'timestamp': item.timestamp.date()
+                'timestamp': item.timestamp
             })
 
+            # Get the hash for the latest run.
             if item.model_output and not git_hash:
                 if 'git_hash' in item.model_output:
                     git_hash = item.model_output['git_hash']
 
-        start = datetime.now().replace(hour=0, minute=0, second=0)
-        end = datetime.now().replace(hour=23, minute=59, second=59)
-        jobs_today = SimulationRun.objects.filter(timestamp__range=[start, end]).count()
+        # Get the total number of jobs today.
+        jobs_today = SimulationRun.objects.filter(
+            timestamp__range=[start, end]
+        ).count()
 
+        # Set the body message.
         body = "Jobs today: {}\nLatest Git Hash: {}".format(jobs_today, git_hash)
 
+        # Write the file.
         with open('/tmp/report.csv', 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=['state', 'counties', 'timestamp'])
             writer.writeheader()
             writer.writerows(rows)
 
+        # Set the email, attach it and send it.
         email = EmailMessage(
             subject='SEIRCast Report',
             body=body,
